0f9c05135ea865e186d1ab0fa42089e964f34e5f
petaboy <nsaldanha@outlook.com>
2016-11-24 14:20:50 -0500

    Removed extraneous files

--- adfdataimport.R
+++
@@ -1,8 +0,0 @@
-#QUandl Data import module for test one.adf . Switch statements will be added to make this module 
-#usable for other tests.
-library(Quandl,tseries)
-ticker <- readline(prompt="Insert stock ticker: ")
-input = paste(c("WIKI/",ticker),collapse='')
-data <-Quandl(input)
-test <- cbind.data.frame(data$Date,data$`Adj. Close`)
-colnames(test) <- c('V1','V2')
